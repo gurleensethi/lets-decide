18f905464844397a2f8163bdb399674c235573b4
Gurleen Sethi <sarusethi@rocketmail.com>
2021-06-29 19:24:54 -0400

    feat: update design.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,6 +3,9 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div> -->
+  <div class="p-4 text-xl border-b">
+    <div class="cursor-pointer" @click="$router.push('/')">Let's Decide</div>
+  </div>
   <div class="p-8 max-w-screen-md mx-auto">
     <router-view />
   </div>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,8 +1,22 @@
 <template>
-  <div>Let's Decide</div>
-  <div class="flex flex-col">
-    <button>Enter a Code</button>
-    <router-link :to="{ name: 'CreateQuestion' }">New Question</router-link>
+  <div class="flex flex-col justify-center items-center">
+    <div class="text-xl mb-4 text-gray-500">
+      Decide something new with your friends 👯‍♂️
+    </div>
+    <router-link
+      class="
+        text-xl
+        bg-purple-500
+        px-4
+        py-2
+        text-white
+        rounded-lg
+        shadow-md
+        mb-6
+      "
+      :to="{ name: 'CreateQuestion' }"
+      >New Question</router-link
+    >
   </div>
 </template>
 
